e16f39679be136dbf1026611598df254d782853b
programadormovel <programadormovel@gmail.com>
2023-04-12 10:10:11 -0300

    projeto spring inicial

---
+++ src/main/java/br/itb/informatica/miniprojeto3dspring/Miniprojeto3dspringApplication.java
@@ -0,0 +1,13 @@
+package br.itb.informatica.miniprojeto3dspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3dspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3dspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/itb/informatica/miniprojeto3dspring/Miniprojeto3dspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.itb.informatica.miniprojeto3dspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3dspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
