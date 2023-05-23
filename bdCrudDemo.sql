2fcd457bd23f15abf588a473c97ef97de25733c9
programadormovel <programadormovel@gmail.com>
2023-05-23 10:34:31 -0300

    ajustes no banco de dados

--- bdCrudDemo.sql
+++ bdCrudDemo.sql
@@ -9,22 +9,6 @@ GO
 USE miniprojeto3d
 GO
 
-CREATE TABLE TipoProduto
-(
-	id		 INT IDENTITY,
-	nomeTipo VARCHAR(100) NOT NULL,  -- ÁGUA, REFRIGERANTE, LANCHE, SALGADO, SALGADINHO, DOCE ou SOBREMESA
-
-	PRIMARY KEY(id)
-)
-GO
-INSERT TipoProduto (nomeTipo) VALUES ('Água')
-INSERT TipoProduto (nomeTipo) VALUES ('Refrigerante')
-INSERT TipoProduto (nomeTipo) VALUES ('Lanche')
-INSERT TipoProduto (nomeTipo) VALUES ('Salgado')
-INSERT TipoProduto (nomeTipo) VALUES ('Salgadinho')
-INSERT TipoProduto (nomeTipo) VALUES ('Doce')
-INSERT TipoProduto (nomeTipo) VALUES ('Sobremesa')
-
 CREATE TABLE Produto
 (
 	id			 INT IDENTITY,
@@ -33,15 +17,17 @@ CREATE TABLE Produto
 	codigoBarras VARCHAR(100) NOT NULL,
 	foto		 varbinary(max) NULL,
 	preco		 DECIMAL(8,2) NOT NULL,
-	tipoProd_id  INT	      NOT NULL,
+	categoria	 VARCHAR(100) NULL,
+	destaque	 VARCHAR(100) NULL,
 	statusProd	 VARCHAR(10)  NOT NULL, -- ATIVO ou INATIVO
-
 	PRIMARY KEY (id),
-	FOREIGN KEY (tipoProd_id) REFERENCES TipoProduto (id)
 )
 GO
-INSERT Produto (nome, descricao, codigoBarras, foto, preco, tipoProd_id, statusProd) 
-VALUES ('Garrafa de Água Mineral de 510ml - Boa-água ', 'Garrafa de água mineral de 510ml', '7899001234', NULL, 2.00, 1, 'ATIVO')
+INSERT Produto (nome, descricao, codigoBarras, foto, 
+preco, categoria, destaque, statusProd) 
+VALUES ('Garrafa de Água Mineral de 510ml - Boa-água ', 
+	'Garrafa de água mineral de 510ml', '7899001234', 
+		NULL, 2.00, 'Bebida', 'promoção', 'ATIVO')
 
 GO
 SELECT * FROM Produto
